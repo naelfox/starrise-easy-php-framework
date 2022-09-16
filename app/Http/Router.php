2d3d77a7aa7272f28ca22312ee8704b432fcbb2d
naelfox <natanaelvila2@gmail.com>
2022-09-16 10:25:15 -0300

    router

--- app/Http/Router.php
+++ app/Http/Router.php
@@ -4,6 +4,52 @@ namespace App\Http;
 
 class Router{
 
+    /**
+     * Full project url (source)
+     * @var string
+     */
     private $url = '';
+
+    /**
+     * commmom prefix
+     * @var string
+     */
+    private $prefix = '';
+
+    /**
+     * route index
+     *
+     * @var array
+     */
+    private $routes = [];
+    
+    /**
+     * Request instance
+     * @var Request
+     */
+
+    private $request;
+
+    /**
+     * method that starts the class
+     * @param string $url
+     */
+    public function __construct($url){
+        $this->request = new Request();
+        $this->url = $url;
+        $this->setPrefix();
+    }
+
+    /**
+     * Define the prefix
+     */
+    private function setPrefix(){
+        //info about actual url
+        $parseUrl = parse_url($this->url);
+        $this->prefix = $parseUrl['path'] ?? '';
+    }
+
+
+
 
 }
--- public/index.php
+++ public/index.php
@@ -2,6 +2,15 @@
 
 require_once "../vendor/autoload.php";
 
+use \App\Http\Router;
 use \App\Controllers\Home;
 
+define('URL', 'http://localhost/mcv');
+
+$obRouter = new Router(URL);
+
+echo '<pre>';
+print_r($obRouter);
+echo '</pre>';
+exit;
 echo Home::getHome();
