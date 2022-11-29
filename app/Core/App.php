b68440b44652b2d063228944c6bdfddc3d167986
naelfox <natanaelvila2@gmail.com>
2022-11-28 22:48:19 -0300

    fix

--- app/Core/App.php
+++ app/Core/App.php
@@ -3,9 +3,7 @@
 use App\Core\View;
 use App\Utils\Url;
 
-
-define('URL', Url::getUrl());
-// define('URL', (new Url())->getUrl());
+define('URL', (new Url())->getUrl());
 
 View::init([
     'URL' => URL
